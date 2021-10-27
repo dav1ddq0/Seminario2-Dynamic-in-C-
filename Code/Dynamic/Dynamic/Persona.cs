db0666a588822c3849c2630665a5cf7e689e1f2a
David Orlando <dquesada969x@gmail.com>
2021-10-27 16:54:50 -0400

    agregado fluent interface mas o menos :(

--- Code/Dynamic/Dynamic/Persona.cs
+++ Code/Dynamic/Dynamic/Persona.cs
@@ -2,6 +2,18 @@ namespace Dynamic
 {
     public class Persona
     {
-        
+
+        public string firstName{get;set;}
+        public string lastName{get;set;}
+        public Persona FirstName(string firstName){
+            
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Persona LastName(string lastName){
+            this.lastName = lastName;
+            return this;
+        }
     }
 }
--- Code/Dynamic/Dynamic/Program.cs
+++ Code/Dynamic/Dynamic/Program.cs
@@ -6,7 +6,11 @@ namespace Dynamic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var p1 = new Persona().FirstName("Ana").LastName("De Armas");
+            var p2 = new Persona().FirstName("Juana").LastName("De Arco");
+            Console.WriteLine($"FirstName: {p1.firstName} LastName: {p1.lastName}");
+            Console.WriteLine($"FirstName: {p2.firstName} LastName: {p2.lastName}");
+            
             
         }
     }
