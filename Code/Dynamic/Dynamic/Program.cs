9b74a24a9f10ec4450223384e3e850c3c66710cb
David Orlando <dquesada969x@gmail.com>
2021-10-27 18:22:34 -0400

    ready to push

--- Code/Dynamic/Dynamic/Program.cs
+++ Code/Dynamic/Dynamic/Program.cs
@@ -7,7 +7,9 @@ namespace Dynamic
         static void Main(string[] args)
         {
             var p1 = Factory.New().Person().FirstName("Juana").LastName("De Arco");
-            Console.WriteLine($"FirstName: {p1.FirstName} LastName: {p1.LastName}");
+            var p2 = Factory.New().Person().FirstName("Ana").LastName("De Armas");
+            Console.WriteLine($"FirstName: {p1.firstName} LastName: {p1.lastName}");
+            Console.WriteLine($"FirstName: {p2.firstName} LastName: {p2.lastName}");
         }
     }
 }
