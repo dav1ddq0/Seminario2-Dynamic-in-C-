874b1447459929a3563519d833e172094ac70853
dmguezjaviersnet <dmguezjaviersnet@gmail.com>
2021-10-29 15:26:00 -0400

    Corregida pregunta 1

---
+++ Code/Dynamic/Dynamic/DynamicObjectCreator.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Reflection;
+using System.Dynamic;
+
+namespace Dynamic {
+
+    public class DynamicObjectCreator : DynamicObject {
+        Assembly execng_code = Assembly.GetExecutingAssembly();
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result) {
+            Type[] class_types = execng_code.GetTypes();
+            
+            foreach (var type in class_types) {
+                if (type.Name == binder.Name) {
+                    Type [] newType = { };
+                    result = Activator.CreateInstance(type);
+                    return true;
+                }
+            }
+
+            result = null;
+            return false;
+        }
+    }
+}
--- Code/Dynamic/Dynamic/Factory.cs
+++ Code/Dynamic/Dynamic/Factory.cs
@@ -1,36 +1,7 @@
-using System.Collections.Generic;
-
-namespace Dynamic
-{
-    //Interfaces
-
-    // public interface IFactoryPersonFirstName {
-    //     IFactoryPersonLastName FirstName (string first_name);
-    // }
-
-    // public interface IFactoryPersonLastName {
-    //     Person LastName (string last_name);
-    // }
-    public static class FactoryExt {
-        public static Person Person(this Factory f, Dictionary<string, string> jsonLikeInput) {
-            return new Person(jsonLikeInput);
-        }
-    }
-
+namespace Dynamic {
     public class Factory {
-        // Person _person;
-        public static Factory New { get { return new Factory(); } }
-        
-        public Person Person { get { return new Person(); } } 
-
-        // public IFactoryPersonLastName FirstName (string first_name) {
-        //     _person.FirstName = first_name;
-        //     return this;
-        // }
-
-        // public Person LastName (string last_name) {
-        //     _person.LastName = last_name;
-        //     return _person;
-        // }
+        public static dynamic New { get { return new DynamicObjectCreator(); } }
     }
+
+    
 }
